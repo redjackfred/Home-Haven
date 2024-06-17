020dd92ce7d24cc0d52842d8360b2389ca29e984
Pei-Wen Tang (Vincent) <redjackfred@gmail.com>
2024-06-17 15:09:04 -0700

    Practice

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,9 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <> <p>New Website</p></>
+    <> 
+      <p>New Website</p>
+      <p>Test for github practicing.</p>
+    </>
   );
 }
