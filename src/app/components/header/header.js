63d0eda99c2ca1fc3c3c18cc36f15b752e29e202
Aaron Lao <91438480+aajmlao@users.noreply.github.com>
2024-06-19 15:46:32 -0700

    add react for header

--- src/app/components/header/header.js
+++ src/app/components/header/header.js
@@ -1,37 +1,45 @@
 import Link from "next/link"
 import logo from "/public/image/header/logo.png"
+
 import headerStyll from "./header.module.css"
+import AltHeader from "./altHeader"
+
 
 export default function MainHeader() {
     return (
-        <header className={headerStyll.header}>
+        <main>
+            <header className={headerStyll.header}>
             <Link className={headerStyll.logo} href="/">
             <img src={logo.src} alt="Home Haven Logo" />
             Home Haven
-        </Link>
+            </Link>
 
-        <nav className={headerStyll.nav}>
-            <ul>
-                <li>
-                    <Link href="/findhomes">Homes</Link>
-                </li>
-                <li>
-                    <Link href="/agents">Agents</Link>
-                </li>
-                <li>
-                    <Link href="/sell">Sell</Link>
-                </li>
-                <li>
-                    <Link href="/buy">Buy</Link>
-                </li>
-                <li>
-                    <Link href="/">My Homes</Link>
-                </li>
-            </ul>
-            
-        </nav>
+            <nav className={headerStyll.nav}>
+                <ul>
+                    <li>
+                        <Link href="/findhomes">Homes</Link>
+                    </li>
+                    <li>
+                        <Link href="/agents">Agents</Link>
+                    </li>
+                    <li>
+                        <Link href="/sell">Sell</Link>
+                    </li>
+                    <li>
+                        <Link href="/buy">Buy</Link>
+                    </li>
+                    <li>
+                        <Link href="/">My Homes</Link>
+                    </li>
+                </ul>
+                
+            </nav>
+            </header>
 
-        </header>
+            <AltHeader/>
+        </main>
+        
+
         
     )
 }
--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import MainHeader from "./components/header/header";
 
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -15,6 +16,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <MainHeader/>
+        
         {children}
       </body>
     </html>
