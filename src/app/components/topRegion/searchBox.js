d9660d25f681199592b3e02d80dbecdc7f8334c0
Pei-Wen Tang (Vincent) <redjackfred@gmail.com>
2024-06-19 21:30:00 -0700

    Fix bugs

--- src/app/components/topRegion/searchBox.js
+++ src/app/components/topRegion/searchBox.js
@@ -1,19 +1,7 @@
 'use client'
-
-import Image from "next/image";
 import styles from "./searchBox.module.css";
-import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 
-export default function SearchBox() {
-  const searchParams = useSearchParams();
-  const params = new URLSearchParams(searchParams);
-  const pathname = usePathname();
-  const { replace } = useRouter();
-
-  function handleSearch(term) {    
-    console.log(term);
-  }
-
+export default function SearchBox() { 
   return (
     <div>     
       <div className={styles.flexContainer}>
@@ -21,8 +9,8 @@ export default function SearchBox() {
           <input type="text" className={styles.inputTxt} placeholder="Enter an address, neighborhood, city, or ZIP code"/>
         </div>
         <div className={styles.searchButton}>
-            <button type="submit" style={{background: 'transparent', border: 'none'}}>              
-            </button>   
+          <button type="submit" style={{background: 'transparent', border: 'none'}}>              
+          </button>   
         </div>        
       </div>
     </div>
--- src/app/components/topRegion/topRegion.js
+++ src/app/components/topRegion/topRegion.js
@@ -1,5 +1,4 @@
 'use client'
-import Image from "next/image";
 import styles from "./topRegion.module.css";
 import InspirationalPhrase from "./inspirationalPhrase";
 import SearchBox from "./searchBox";
