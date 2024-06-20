45d6da7b1f7a13430c29916116f7a23188628a74
Pei-Wen Tang (Vincent) <redjackfred@gmail.com>
2024-06-19 22:03:06 -0700

    Fix

--- src/app/components/header/header.js
+++ src/app/components/header/header.js
@@ -2,10 +2,10 @@ import Link from "next/link"
 import Image from "next/image"
 import headerStyll from "./header.module.css"
 import AltHeader from "./altHeader"
-import Image from "next/image"
 
 export default function MainHeader() {
     return (
+        <>
         <header className={headerStyll.header}>
             <Link className={headerStyll.logo} href="/">            
             <Image src="/image/header/logo.png" 
