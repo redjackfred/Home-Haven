792562beb408eb5518e0b44f721947fc94ba91bb
Aaron Lao <91438480+aajmlao@users.noreply.github.com>
2024-06-23 21:03:24 -0700

    stashed changed

---
+++ src/app/components/TypeCard/typeCard.js
@@ -0,0 +1,11 @@
+"use client"
+import { useState } from "react"
+import cssStyle from "./typeCard.module.css"
+
+export default function TypeCard() {
+    return (
+        <>
+        <h4 className={cssStyle.title}>How can we help?</h4>
+        </>
+    )
+}
