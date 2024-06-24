ebd3c8fd0b2111273bb1c4673fbd98615fa710ec
Pei-Wen Tang (Vincent) <redjackfred@gmail.com>
2024-06-23 21:27:30 -0700

    Good

--- src/app/page.js
+++ src/app/page.js
@@ -2,24 +2,39 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import AssetCard from "./components/assetCard/assetCard";
 import TopRegion from "./components/topRegion/topRegion";
-import TypeCard from "./components/TypeCard/typeCard";
+import { Typography } from "@mui/material";
 
-export default function Home() {
+
+export default function Home() { 
   return (
     <div>           
       <TopRegion/>
-      <AssetCard
-                imgData={"/image/assetCard/house.png" }
-                imgAlt="Placeholder Image"
-                date="4 Feb, 2024"
-                price="$40,999,999"
-                numberOfBedrooms="3"
-                numberOfBaths="2"
-                numberOfSqft= "1,568"
-                address= "22055 White Stone Road, Marysville OH"
-      />
-      <TypeCard />
-      
+      <div className={styles.newestListings}>
+        <div className={styles.title}>
+          <Typography variant="h3">
+            Newest listings
+          </Typography>         
+        </div>
+        <div className={styles.listingList}>
+          <AssetCard
+                    imgData={"/image/assetCard/house.png" }
+                    imgAlt="Placeholder Image"
+                    date="4 Feb, 2024"
+                    price="$40,999,999"
+                    numberOfBedrooms="3"
+                    numberOfBaths="2"
+                    numberOfSqft= "1,568"
+                    address= "22055 White Stone Road, Marysville OH"
+          />
+        </div>
+      </div>
+      <div className={styles.howCanWeHelp}>
+        <div className={styles.title}>
+          <Typography variant="h3">
+            How can we help?
+          </Typography>
+        </div>        
+      </div>
     </div>
   );
 }
