4f672a1b1372d89279f0c16378db794f02e1a3d8
Pei-Wen Tang (Vincent) <redjackfred@gmail.com>
2024-07-07 12:13:30 -0700

    fix searchbox overflow

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,7 +8,6 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import NavBar from "./components/sidebar/navBar";
 import SidebarHeader from "./components/sidebar/sidebarHeader";
 
 const inter = Inter({ subsets: ["latin"] });
