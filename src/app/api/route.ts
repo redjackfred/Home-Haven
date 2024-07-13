250dbf669d35ea86be2a5b5f56690176377b8f84
Pei-Wen Tang (Vincent) <redjackfred@gmail.com>
2024-07-12 20:40:46 -0700

    Connect to MongoDB

---
+++ src/app/api/route.ts
@@ -0,0 +1,18 @@
+import {connectToDatabase} from "../lib/mongodb";
+
+export async function GET(request: Request) {
+  try{
+    const { db } = await connectToDatabase();
+          
+    const posts = await db
+      .collection("home_informations")
+      .find({})
+      .limit(5)
+      .toArray();
+
+    return Response.json({ posts });
+  } catch (e) {
+    console.error(e);
+    return Response.json({ error: e });
+  } 
+}
