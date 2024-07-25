7f88cdeced3a76b0bda200fa84e94f376d08ab49
Pei-Wen Tang (Vincent) <redjackfred@gmail.com>
2024-07-24 21:09:50 -0700

    search address can show the results from that zip code

--- src/app/components/searchBox/newSearchBox.js
+++ src/app/components/searchBox/newSearchBox.js
@@ -1,13 +1,13 @@
-'use client'
+"use client";
 import SearchIcon from "@mui/icons-material/Search";
 import { useRef, useState } from "react";
 import styles from "./newSearchBox.module.css";
 import { usePathname, useRouter } from "next/navigation";
-import json2mq from 'json2mq';
+import json2mq from "json2mq";
 import { useMediaQuery } from "@mui/material";
 
 export default function NewSearchBox() {
-  const router = useRouter()
+  const router = useRouter();
   const pathName = usePathname();
   const [inputValue, setInputValue] = useState("");
   const searchInput = useRef(null);
@@ -16,42 +16,78 @@ export default function NewSearchBox() {
     searchInput.current.classList.add(styles.focus);
   }
 
-  function handleInputUnFocus(){
+  function handleInputUnFocus() {
     searchInput.current.classList.remove(styles.focus);
   }
 
-
-  function handleOnChange(event){
+  function handleOnChange(event) {
     const { value } = event.target;
     setInputValue(value);
   }
 
-  function handleKeyPress(event){
-    if(event.key === "Enter"){
+  function handleKeyPress(event) {
+    if (event.key === "Enter") {
       event.preventDefault();
       return handleSearch();
     }
   }
 
-  function handleSearch(){    
-    return router.push(`${pathName}/?q=${inputValue}`);     
+ 
+  function handleSearch() {
+    if(inputValue.length > 5){
+      let zipCode = -1;
+      let geocoder;
+      geocoder = new google.maps.Geocoder();
+      geocoder.geocode({ address: inputValue }, function (results, status) {
+        if (status == "OK" && results[0].address_components[7]) {       
+          zipCode = results[0].address_components[7].short_name;     
+        } 
+      }).then(() => {
+        return router.push(`${pathName}/?q=${zipCode}`);
+      });     
+    }else{
+      return router.push(`${pathName}/?q=${inputValue}`);
+    }
+    
   }
 
   const matches = useMediaQuery(
-    json2mq({        
+    json2mq({
       minWidth: 431,
-    }),
-  );  
+    })
+  );
+
   
+  function codeAddress(address) {
+    geocoder = new google.maps.Geocoder();
+    geocoder.geocode({ address: address }, function (results, status) {
+      if (status == "OK") {       
+        zipCode = results[0].address_components[7].short_name;       
+      } 
+    });
+  }
 
   return (
     <form className={styles.form}>
-      <input type="text" className={styles.input} value={inputValue?? ""} onFocus={handleInputFocus} onBlur={handleInputUnFocus} onChange={handleOnChange} onKeyDown={handleKeyPress} placeholder={matches ? "Enter an address, neighborhood, city, or ZIP code" : "Enter an address"} />      
+      <input
+        type="text"
+        className={styles.input}
+        value={inputValue ?? ""}
+        onFocus={handleInputFocus}
+        onBlur={handleInputUnFocus}
+        onChange={handleOnChange}
+        onKeyDown={handleKeyPress}
+        placeholder={
+          matches
+            ? "Enter an address, neighborhood, city, or ZIP code"
+            : "Enter an address"
+        }
+      />
       <div className={styles.icon} ref={searchInput}>
         <SearchIcon
           sx={{
             color: "white",
-            backgroundColor: "#14B49C",          
+            backgroundColor: "#14B49C",
             width: "35px",
             height: "35px",
           }}
--- src/app/sell/page.js
+++ src/app/sell/page.js
@@ -1,12 +1,14 @@
 "use client";
-import { useEffect, useState } from "react";
+import { use, useEffect, useState } from "react";
 import NewSearchBox from "../components/searchBox/newSearchBox";
 import AssetCard from "../components/assetCard/assetCard";
 import styles from "./page.module.css";
 import { useSearchParams } from "next/navigation";
+import { APIProvider } from "@vis.gl/react-google-maps";
 
 export default function Sell() {
   const [homes, setHomes] = useState([]);
+  const [zipCode, setZipCode] = useState(-1);
   const searchParams = useSearchParams();
 
   const searchQuery = searchParams && searchParams.get("q");
@@ -24,51 +26,68 @@ export default function Sell() {
   useEffect(() => {
     getHomes();
   }, []);
+  
 
-  const filteredHomes = homes.filter((home) => {   
-    const numericQuery = parseFloat(searchQuery);
-    const isNumeric = !isNaN(numericQuery);   
-    
-    if(searchQuery){  
-         // If the search query is numeric, check numerical fields  
-        if (isNumeric) {  
-            return home.bathrooms.toString() === searchQuery || home.bedrooms.toString() === searchQuery;  
-        } else {  
-            // Search in string fields  
-            const lowerSearchQuery = searchQuery.toLowerCase();  
-            return home.address.toLowerCase().includes(lowerSearchQuery) ||  
-                   home.city.toLowerCase().includes(lowerSearchQuery);  
-        }           
-    }    
+  const filteredHomes = homes.filter((home) => {
+    let isNum = /^\d+$/.test(searchQuery);
+
+    if (searchQuery) {
+      // If the search query is numeric, check numerical fields
+      if (isNum) {
+        // Bath room and bed room search
+        if (searchQuery.length < 2) {
+          return (
+            home.bathrooms.toString() === searchQuery ||
+            home.bedrooms.toString() === searchQuery
+          );
+          // Zip code search
+        } else if (searchQuery.length === 5) { 
+          return home.zip_code == searchQuery;
+        }
+      } else {
+        // Search in string fields
+        const lowerSearchQuery = searchQuery.toLowerCase();
+        return (
+          home.address.toLowerCase().includes(lowerSearchQuery) ||
+          home.city.toLowerCase().includes(lowerSearchQuery)
+        );
+      }
+    }
     return homes;
   });
 
   return (
     <>
-      <div className={styles.container}>
-        <h1>Test page for searching</h1>
-        <br />
-        <NewSearchBox />
-        <ul className={styles.posts}>
-          {filteredHomes.length > 0 ? (
-            filteredHomes.map((home) => (
-              <AssetCard
-                imgData={home.image_url[0]}
-                imgAlt="Placeholder Image"
-                date="4 Feb, 2024"
-                price={home.price.toString()}
-                numberOfBedrooms={home.bedrooms}
-                numberOfBaths={home.bathrooms}
-                numberOfSqft={home.square_feet.toString()}
-                address={home.address}
-                key={home._id}
-              />
-            ))
-          ) : (
-            <p>No homes found for the query: {searchQuery}</p>
-          )}
-        </ul>
-      </div>
+      <APIProvider
+        apiKey={"AIzaSyCIm_MVTHuuOneXJhD16L4NZ2TOWdew07o"}
+        onLoad={() => console.log("Maps API has loaded.")}
+        libraries={["marker"]}
+      >        
+        <div className={styles.container}>
+          <h1>Test page for searching</h1>
+          <br />
+          <NewSearchBox />
+          <ul className={styles.posts}>
+            {filteredHomes.length > 0 ? (
+              filteredHomes.map((home) => (
+                <AssetCard
+                  imgData={home.image_url[0]}
+                  imgAlt="Placeholder Image"
+                  date="4 Feb, 2024"
+                  price={home.price.toString()}
+                  numberOfBedrooms={home.bedrooms}
+                  numberOfBaths={home.bathrooms}
+                  numberOfSqft={home.square_feet.toString()}
+                  address={home.address}
+                  key={home._id}
+                />
+              ))
+            ) : (
+              <p className={styles.noHome}>No homes found for the query: {searchQuery}</p>
+            )}
+          </ul>
+        </div>
+      </APIProvider>
     </>
   );
 }
