6776f83790a8bf681629dd8691a2665bf0b100ae
Pei-Wen Tang (Vincent) <redjackfred@gmail.com>
2024-07-29 13:07:45 -0700

    search and filter

--- src/app/components/filterButton/filterButton.tsx
+++ src/app/components/filterButton/filterButton.tsx
@@ -1,90 +1,29 @@
-import * as React from 'react';
+import { useState } from 'react';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
-import FormHelperText from '@mui/material/FormHelperText';
 import FormControl from '@mui/material/FormControl';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
-import { Slider } from '@mui/material';
 import { Typography } from "@mui/material";
 import useMediaQuery from '@mui/material/useMediaQuery';
 import json2mq from "json2mq";
 
 
 
-export default function FilterButton({
-
-  // props here
-  // e.g. title,
+export default function FilterButton({  
   title,
+  name,
   items,
-  multiple,
-  // e.g. onClick
-  // e.g. children
-  // e.g. style
-  // e.g. className
-  // e.g. etc.
-  // e.g. ...rest
-  // e.g. ...props
-  // e.g. ...other
-}: {
-  // props types here
-  // e.g. title: string,
+  values,
+  onSelectionChange,
+}: { 
   title: string,
-  items?: string[],
-  multiple?: boolean,
-  // e.g. onClick: () => void,
-  // e.g. children: React.ReactNode,
-  // e.g. style: React.CSSProperties,
-  // e.g. className: string,
-  // e.g. etc.
-  // e.g. ...rest: any
-  // e.g. ...props: any
-  // e.g. ...other
-}) {
-  const [age, setAge] = React.useState('');
-  const [values, setValues] = React.useState([]);
+  name: string,
+  items?: string[], 
+  values: Object,
+  onSelectionChange?: (selected: SelectChangeEvent<string>) => void
+}) { 
   const max430 = useMediaQuery(json2mq({maxWidth: 430}));
-  const handleChanges = (event: SelectChangeEvent<typeof values>) => {
-    const {
-      target: { value },
-    } = event;
-    setValues(
-      // On autofill we get a stringified value.
-      typeof value === 'string' ? value.split(',') : value,
-    );
-  };
-
-
-  const handleChange = (event: SelectChangeEvent) => {
-    setAge(event.target.value);
-  };
-
-  const marks = [
-    {
-      value: 0,
-      label: '0°C',
-    },
-    {
-      value: 20,
-      label: '20°C',
-    },
-    {
-      value: 37,
-      label: '37°C',
-    },
-    {
-      value: 100,
-      label: '100°C',
-    },
-  ];
-
-function valuetext(value: number) {
-  return `${value}°C`;
-}
-  const theme ={
-    spacing: 8,
-  }
-
+  
   return (
     <FormControl sx={max430 ? { m: 0, width: 104, maxHeight: 48, height: 39}: {m: 0, width: 140, maxHeight: 48, height: 48}}>
       <InputLabel id="select-label"  sx={max430?{m:-0.8, ml:0.3, color:"#0B3650"}: {color:"#0B3650"}}>
@@ -94,15 +33,15 @@ function valuetext(value: number) {
       </InputLabel>
       <Select sx={max430 ? { height: 39 }: {height: 48}}
         labelId="select-label"
-        id="select-label"
-        multiple={multiple}
-        value={multiple ? values : age}
+        id="select-label"    
+        name={title}     
+        value={values[name]}
         label={title}
-        onChange={multiple ? handleChanges : handleChange}
+        onChange={onSelectionChange}
       >        
         {
           items?.map((item) => (
-            <MenuItem key={item} value={item}>{item}</MenuItem>
+            <MenuItem key={item} value={item}>{item === "" ? "None" : item}</MenuItem>
           ))
         }        
       </Select>
--- src/app/findhomes/page.tsx
+++ src/app/findhomes/page.tsx
@@ -1,179 +1,325 @@
-'use client'
-import { Typography } from "@mui/material";
+"use client";
+import { SelectChangeEvent, Typography } from "@mui/material";
 import SearchBox from "../components/searchBox/searchBox";
-import styles from "./page.module.css"
-import { AdvancedMarker, APIProvider, InfoWindow, Map, Marker, Pin } from '@vis.gl/react-google-maps';
+import styles from "./page.module.css";
+import {
+  AdvancedMarker,
+  APIProvider,
+  InfoWindow,
+  Map,
+  Marker,
+  Pin,
+} from "@vis.gl/react-google-maps";
 import PoiMarkers from "./poiMarker";
 import Image from "next/image";
 import FilterButton from "../components/filterButton/filterButton";
 import SidebarHeader from "../components/sidebar/sidebarHeader";
 import Link from "next/link";
-import styled, {css} from "styled-components";
+import styled, { css } from "styled-components";
 import { useEffect, useState, useRef } from "react";
 import ToggleButtons from "../components/toggleComponents/toggleButton";
 import HomeListing from "../components/homeListing/homeListing";
 import windowsize from "../components/getWindowSize/getWindowSize";
 import useMediaQuery from "@mui/material/useMediaQuery";
+import NewSearchBox from "../components/searchBox/newSearchBox";
 
-type Poi = { key: string, location: google.maps.LatLngLiteral }
+type Poi = { key: string; location: google.maps.LatLngLiteral };
 const locations: Poi[] = [
-    { key: 'paintedLadies', location: { lat: 37.77620759443369, lng: -122.43277996388103 } },
-    { key: 'USF', location: { lat: 37.7765906497719, lng: -122.45033721057825 } },
-    { key: 'traderJoes', location: { lat: 37.78335565014062, lng: -122.44776777198993 } },
-    { key: 'chinaTown', location: { lat: 37.7963061525071, lng: -122.40536900230657 } },
-    { key: 'wholeFoods', location: { lat: 37.76944978354186, lng: -122.45296457611452 } },
-    { key: 'goodWill', location: { lat: 37.76969532433501, lng: -122.4504232309301 } },
-    { key: 'goldenGatePark', location: { lat: 37.770555211469635, lng: -122.46750884294708 } },
-    { key: 'castro', location: { lat: 37.76091977542431, lng: -122.43499102109526 } },
-    { key: 'sunset', location: { lat: 37.7521173057647, lng: -122.48671685777316 } },
-    { key: 'chaseCenter', location: { lat: 37.76806975668889, lng: -122.38773616870792 } },
-    { key: 'sunset2', location: { lat: 37.75384480789679, lng: -122.48380366818245 } },
-    { key: 'sunset3', location: { lat: 37.75254918511742, lng: -122.4925432374971 } },
+  {
+    key: "paintedLadies",
+    location: { lat: 37.77620759443369, lng: -122.43277996388103 },
+  },
+  { key: "USF", location: { lat: 37.7765906497719, lng: -122.45033721057825 } },
+  {
+    key: "traderJoes",
+    location: { lat: 37.78335565014062, lng: -122.44776777198993 },
+  },
+  {
+    key: "chinaTown",
+    location: { lat: 37.7963061525071, lng: -122.40536900230657 },
+  },
+  {
+    key: "wholeFoods",
+    location: { lat: 37.76944978354186, lng: -122.45296457611452 },
+  },
+  {
+    key: "goodWill",
+    location: { lat: 37.76969532433501, lng: -122.4504232309301 },
+  },
+  {
+    key: "goldenGatePark",
+    location: { lat: 37.770555211469635, lng: -122.46750884294708 },
+  },
+  {
+    key: "castro",
+    location: { lat: 37.76091977542431, lng: -122.43499102109526 },
+  },
+  {
+    key: "sunset",
+    location: { lat: 37.7521173057647, lng: -122.48671685777316 },
+  },
+  {
+    key: "chaseCenter",
+    location: { lat: 37.76806975668889, lng: -122.38773616870792 },
+  },
+  {
+    key: "sunset2",
+    location: { lat: 37.75384480789679, lng: -122.48380366818245 },
+  },
+  {
+    key: "sunset3",
+    location: { lat: 37.75254918511742, lng: -122.4925432374971 },
+  },
 ];
 
-
 const SignIn = styled.div`
-    display: flex;
-    align-items: center;
-    color : #14B49C;
-    margin-left: 8px;
-    width: 44px;
+  display: flex;
+  align-items: center;
+  color: #14b49c;
+  margin-left: 8px;
+  width: 44px;
 `;
 
-
 const FilterButtonContainer = styled.div`
-    display: none;
-    position: absolute;
-    top: 20px;
-    right: 24px;
-    border-radius: 5px;
-    border: #14B49C solid 1px; 
-    background-color: #EFFFFC;
-    z-index: 2;
-    @media (max-width: 800px) {
-        display: block;
-    }  
+  display: none;
+  position: absolute;
+  top: 20px;
+  right: 24px;
+  border-radius: 5px;
+  border: #14b49c solid 1px;
+  background-color: #effffc;
+  z-index: 2;
+  @media (max-width: 800px) {
+    display: block;
+  }
 `;
 
 export default function FindHomes() {
-    const [lattitude, setLattitude] = useState(37.766338623365684);
-    const [longitude, setLongitude] = useState(-122.44773195354642);
-    const [focus, setFocus] = useState(12.5);
-    const [isload, setIsload] = useState(false);
-    const [isMapButtonActive, setIsMapButtonActive] = useState(true); //lift up states between components.
-    const max1502 = useMediaQuery("(max-width:1502px)");
+  const [lattitude, setLattitude] = useState(37.766338623365684);
+  const [longitude, setLongitude] = useState(-122.44773195354642);
+  const [focus, setFocus] = useState(12.5);
+  const [isload, setIsload] = useState(false);
+  const [isMapButtonActive, setIsMapButtonActive] = useState(true); //lift up states between components.
+  const max1502 = useMediaQuery("(max-width:1502px)");
+  const [filterValues, setFilterValues] = useState({
+    Price: "",
+    Beds: "",
+    Baths: "",
+    HomeType: "",
+    More: "",
+  });
 
+  const handleFilterButtonClick = (event: SelectChangeEvent<string>) => {
+    const { name, value } = event.target;
 
-    const handleMapButtonClick = () =>{
-        setIsMapButtonActive(!isMapButtonActive);
-    }
+    setFilterValues((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
 
-    const handleMarkerClick = (latitude, longitude) => {
-        // Update the map center and zoom based on the marker clicked
-        setLattitude(latitude);
-        setLongitude(longitude);
-        // Update the zoom level if necessary
-        setFocus(13.5);
-    };
-    const{width, height} = windowsize();
+  const handleMapButtonClick = () => {
+    setIsMapButtonActive(!isMapButtonActive);
+  };
 
-    useEffect(() => {
-        setTimeout(()=>{
-            setIsload(true);
-        }, 100);
-        
-    },[]);
+  useEffect(() => {
+    setTimeout(() => {
+      setIsload(true);
+    }, 100);
+  }, []);
 
-    const newStyle ={
-        mapContainer :{
-            width: "70%",
-        }
-    }
- 
-    
-    let mapDisplayString = "";
-    if(!isMapButtonActive) {
-        mapDisplayString = "none";
-    }
-    
-    // console.log(isMapButtonActive);
-    return (
-        <div className={styles.container}>    
-            <APIProvider apiKey={"AIzaSyCIm_MVTHuuOneXJhD16L4NZ2TOWdew07o"} onLoad={() => console.log('Maps API has loaded.')} libraries={['marker']}>         
-                <div className={styles.searchFilterContainer}>
-                    {isload ? 
-                    <div className={styles.searchContainer}>
-                        <SidebarHeader findhomes={true}/>
-                        <Link href="/" >
-                            <Image src="/image/header/logo.png" 
-                                alt="Home Haven Logo"                           
-                                width={32}
-                                height={32}
-                                className={styles.logo}
-                            /> 
-                        </Link>                       
-                        <SearchBox displayBorder/>
-                        <Link href="/signIn" className={styles.signIn}>
-                            <Typography variant="subtitle2">
-                                <SignIn>
-                                    Sign in
-                                </SignIn>
-                            </Typography>
-                        </Link>
-                    </div>: ""}
-                    {/* Add dropdown buttons here*/}
-                    <div className={styles.filterButtonContainer}>
-                        <FilterButton title="Price" items={["0-1000", "1000-2000", "2000-3000", "3000-4000", "4000-5000", "4000-5000", "5000-6000", "6000-7000"]} />
-                        <FilterButton title="Beds & Baths" items={["1B/1B", "1B/2B", "2B/1B", "2B/2B", "1B/3B", "3B/1B", "3B/3B"]} />
-                        <FilterButton title="Home Type" items={["Single Family House", "Condo", "Apartment", "Duplex", "Town House"]} />
-                        <FilterButton title="More" multiple={true} items={["Allow Dogs", "Allow Cats", "In-unit laundry", "Microwaver", "A/C included", "Has basement", "Mountain view", "Park view", "Water view"]} />
-                    </div>
-                </div>
-                <div className={styles.contentContainer}>
-                    <ToggleButtons OnMapButtonClick={handleMapButtonClick} mapStatus={isMapButtonActive}/> {/* This is a toggleButtons on the google map */}
-                    <FilterButtonContainer>
-                        {isload? 
-                            <FilterButton title="Filters" items={["Price", "Beds & Baths", "Home Type", "More"]} /> : ""}
-                    </FilterButtonContainer>      
-                        <div id="mapContainer" > 
-                        <Map 
-                            mapId={'bf51a910020fa25a'}
-                            defaultZoom={focus}
-                            defaultCenter={{ lat: lattitude, lng: longitude }}
-                            gestureHandling={'greedy'}
-                            disableDefaultUI>
-                            <PoiMarkers pois={locations}/>                           
-                        </Map>
-                        {/* NextJs'way to write css */}
-                        </div>
-                        <style jsx>
-                            {`#mapContainer{
-                                width: 50%;
-                            }
-                            @media (max-width: 1502px) {
-                                #mapContainer{
-                                    width: 70%;
-                                }
-                            }
-                            @media (max-width: 800px) {
-                                #mapContainer{
-                                display: ${mapDisplayString};
-                                width: 100%;
-                                }
-                            }
-                            @media (max-width: 430px) {
-                                #mapContainer{
-                                display: ${mapDisplayString};
-                                width: 100%;
-                                }
-                            }
-                            `}
-                        </style>
-               
-                    
-                    <HomeListing onListButton={!isMapButtonActive}/>
-                </div>
-            </APIProvider>
+  const newStyle = {
+    mapContainer: {
+      width: "70%",
+    },
+  };
+
+  let mapDisplayString = "";
+  if (!isMapButtonActive) {
+    mapDisplayString = "none";
+  }
+
+  // console.log(isMapButtonActive);
+  return (
+    <div className={styles.container}>
+      <APIProvider
+        apiKey={"AIzaSyCIm_MVTHuuOneXJhD16L4NZ2TOWdew07o"}
+        onLoad={() => console.log("Maps API has loaded.")}
+        libraries={["marker"]}
+      >
+        <div className={styles.searchFilterContainer}>
+          {isload ? (
+            <div className={styles.searchContainer}>
+              <SidebarHeader findhomes={true} />
+              <Link href="/">
+                <Image
+                  src="/image/header/logo.png"
+                  alt="Home Haven Logo"
+                  width={32}
+                  height={32}
+                  className={styles.logo}
+                />
+              </Link>
+              <NewSearchBox />
+              {/* <SearchBox displayBorder/> */}
+              <Link href="/signIn" className={styles.signIn}>
+                <Typography variant="subtitle2">
+                  <SignIn>Sign in</SignIn>
+                </Typography>
+              </Link>
+            </div>
+          ) : (
+            ""
+          )}
+          {/* Add dropdown buttons here*/}
+          <div className={styles.filterButtonContainer}>
+            <FilterButton
+              title="Price"
+              name="Price"
+              items={[
+                "",
+                "0-100000",
+                "100000-500000",
+                "500000-1000000",
+                "1000000-1500000",
+                "1500000-2000000",
+                "2000000-2500000",
+                "2500000-3000000",
+                "3000000-3500000",
+                "3500000-4000000",
+              ]}
+              values={filterValues}
+              onSelectionChange={handleFilterButtonClick}
+            />
+            <FilterButton
+              title="Beds"
+              name="Beds"
+              items={[
+                "",
+                "1",
+                "2",
+                "3",
+                "4",
+                "5",
+                "6",
+                "7",
+                "8",
+                "9",
+                "10",
+              ]}
+              values={filterValues}
+              onSelectionChange={handleFilterButtonClick}
+            />
+            <FilterButton
+              title="Baths"
+              name="Baths"
+              items={[
+                "",
+                "1",
+                "2",
+                "3",
+                "4",
+                "5",
+                "6",
+                "7",
+                "8",
+                "9",
+                "10",
+              ]}
+              values={filterValues}
+              onSelectionChange={handleFilterButtonClick}
+            />
+            <FilterButton
+              title="Home Type"
+              name="HomeType"
+              items={[
+                "",
+                "Single Family House",
+                "Condo",
+                "Apartment",
+                "Duplex",
+                "Town House",
+              ]}
+              values={filterValues}
+              onSelectionChange={handleFilterButtonClick}
+            />
+            <FilterButton
+              title="More"
+              name="More"
+              items={[
+                "",
+                "Allow Dogs",
+                "Allow Cats",
+                "In-unit laundry",
+                "Microwaver",
+                "A/C included",
+                "Has basement",
+                "Mountain view",
+                "Park view",
+                "Water view",
+              ]}
+              values={filterValues}
+              onSelectionChange={handleFilterButtonClick}
+            />
+          </div>
         </div>
-    );
+        <div className={styles.contentContainer}>
+          <ToggleButtons
+            OnMapButtonClick={handleMapButtonClick}
+            mapStatus={isMapButtonActive}
+          />{" "}
+          {/* This is a toggleButtons on the google map */}
+          <FilterButtonContainer>
+            {isload ? (
+              <FilterButton
+                title="Filters"
+                name="filters"
+                items={["Price", "Beds & Baths", "Home Type", "More"]}
+                values={filterValues}
+              />
+            ) : (
+              ""
+            )}
+          </FilterButtonContainer>
+          <div id="mapContainer">
+            <Map
+              mapId={"bf51a910020fa25a"}
+              defaultZoom={focus}
+              defaultCenter={{ lat: lattitude, lng: longitude }}
+              gestureHandling={"greedy"}
+              disableDefaultUI
+            >
+              <PoiMarkers pois={locations} />
+            </Map>
+            {/* NextJs'way to write css */}
+          </div>
+          <style jsx>
+            {`
+              #mapContainer {
+                width: 50%;
+              }
+              @media (max-width: 1502px) {
+                #mapContainer {
+                  width: 70%;
+                }
+              }
+              @media (max-width: 800px) {
+                #mapContainer {
+                  display: ${mapDisplayString};
+                  width: 100%;
+                }
+              }
+              @media (max-width: 430px) {
+                #mapContainer {
+                  display: ${mapDisplayString};
+                  width: 100%;
+                }
+              }
+            `}
+          </style>
+          <HomeListing onListButton={!isMapButtonActive} filterValues={filterValues}/>
+        </div>
+      </APIProvider>
+    </div>
+  );
 }
